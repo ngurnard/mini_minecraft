c6c5aca6bf5f1aae3e8827fb20a8b3a0c5968535
ngurnard <nk.gurnard@gmail.com>
2022-11-30 18:24:22 -0500

    Apply lighting only to top of water surface. Turn off distance fog.

--- assignment_package/glsl/lambert.frag.glsl
+++ assignment_package/glsl/lambert.frag.glsl
@@ -95,6 +95,8 @@ void main()
 
     float specularIntensity = 0;
 
+    vec4 fs_Nor_new = fs_Nor;
+
     if (fs_Anim != 0) {
         // check region in texture to decide which animatable type is drawn
         bool lava = fs_UVs.x >= 13.f/16.f && fs_UVs.y < 2.f/16.f;
@@ -133,22 +135,14 @@ void main()
             vec4 lightDir = normalize(fs_LightVec - fs_Pos); // vector from the fragment's world-space position to the light source (assuming a point light source)
             vec4 viewDir = normalize(fs_CamPos - fs_Pos); // vector from the fragment's world-space position to the camera
             vec4 halfVec = normalize(lightDir + viewDir); // vector halfway between light source and view direction
+            // float shininess = 10.f;
             float shininess = 400.f;
             specularIntensity = max(pow(dot(halfVec, normalize(fs_Nor)), shininess), 0);
         }
     }
 
-//    // ----------------------------------------------------
-//    // Blinn-Phong Shading
-//    // ----------------------------------------------------
-//    vec4 lightDir = normalize(fs_LightVec - fs_Pos); // vector from the fragment's world-space position to the light source (assuming a point light source)
-//    vec4 viewDir = normalize(fs_CamPos - fs_Pos); // vector from the fragment's world-space position to the camera
-//    vec4 halfVec = normalize(lightDir + viewDir); // vector halfway between light source and view direction
-//    float shininess = 400.f;
-//    specularIntensity = max(pow(dot(halfVec, normalize(fs_Nor)), shininess), 0);
-
     // Calculate the diffuse term for Lambert shading
-    float diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
+    float diffuseTerm = dot(normalize(fs_Nor_new), normalize(fs_LightVec));
 
     // Avoid negative lighting values
     diffuseTerm = clamp(diffuseTerm, 0, 1);
